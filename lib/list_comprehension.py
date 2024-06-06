a10327b026561ef92d2ea36f3eb44abaeb9615c5
Alex Karanja <alexnjorge953@gmail.com>
2024-06-06 15:54:51 +0300

    comprehension changes

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_numbers = []
+    for num in num_list:
+        if num % 2 == 0:
+            even_numbers.append(num)
+    return even_numbers
+
+def return_evens(num_list):
+    return [n for n in num_list if n%2 == 0]
 
 def make_exclamation(sentence_list):
+    return [sentence + "!" for sentence in sentence_list]
     pass
